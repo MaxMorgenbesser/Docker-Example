6d040f55dba166e3d04fed58fb249ba7962d419e
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-08-22 11:01:34 -0400

    docker-example

--- api.ts
+++ api.ts
@@ -1,19 +1,23 @@
 import express, { Request, Response } from "express"
 import cors from "cors"
 
+
 const app = express();
 app.use(express.json())
 app.use(cors())
 const port = 8000;
 
+
 app.get("/",(req: Request, res: Response) => {
-    res.status(200).send("Express in docker is working...And now with logs")
+    res.status(200).send("Choose what you wish")
 })
+
 
 app.get("/todo",(req: Request, res: Response) => {
     const todo = ["take out garbage", "do dishes", "go shopping for a new iphone"]
     res.status(200).json(todo)
 })
+
 
 app.listen(port,()=> {
     console.log("Started on localhost:",port)
